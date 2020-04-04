97cafd5a69846ec6cd479c95c6b86177aeef8e02
Elegater <etedwin1234@gmail.com>
2020-04-05 01:35:28 +0800

    First commit

---
+++ ApexToJson/Apex.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApexToJson
+{
+    class Apex
+    {
+        public Apex(string id, string ship, string rank, string apexName, string type, string description, string cost, string dps, string mainType, string aura, string zen, string video, string imageName)
+        {
+            this.id = id;
+            this.ship = ship;
+            this.rank = rank;
+            this.apexName = apexName;
+            this.type = type;
+            this.description = description;
+            this.cost = cost;
+            this.dps = dps;
+            this.mainType = mainType;
+            this.aura = aura;
+            this.zen = zen;
+            this.video = video;
+            this.imageName = imageName;
+        }
+
+        public string id { get; set; }
+        public string ship { get; set; }
+        public string rank { get; set; }
+        public string apexName { get; set; }
+        public string type { get; set; }
+        public string description { get; set; }
+        public string cost { get; set; }
+        public string dps { get; set; }
+        public string mainType { get; set; }
+        public string aura { get; set; }
+        public string zen { get; set; }
+        public string video { get; set; }
+        public string imageName { get; set; }
+
+    }
+}
---
+++ ApexToJson/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OfficeOpenXml;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ApexToJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "./Data.xlsx";
+            int startRow = 2;
+            int startCol = 1;
+
+            FileInfo fi = new FileInfo(filePath);
+            ExcelRange data = null;
+            List<Apex> apexes = new List<Apex>();
+
+            if (!File.Exists(fi.FullName))
+            {
+                Console.WriteLine("Incorrect file name");
+                return;
+            }
+            using (ExcelPackage p = new ExcelPackage(fi))
+            {
+                ExcelWorkbook wb = p.Workbook;
+                ExcelWorksheet ws = wb.Worksheets[1];
+                data = ws.Cells;
+
+                int endRow = 0;
+                int endCol = 0;
+                bool lastRowFound = false;
+                bool lastColFound = false;
+
+                for (int i = startRow; !lastRowFound; i++)
+                {
+                    if (String.IsNullOrEmpty(ws.Cells[i, startCol].Text))
+                    {
+                        lastRowFound = true;
+                    }
+                    else
+                    {
+                        endRow = i;
+                    }
+                }
+
+                for (int i = startRow; !lastColFound; i++)
+                {
+                    if (String.IsNullOrEmpty(ws.Cells[startRow, i].Text))
+                    {
+                        lastColFound = true;
+                    }
+                    else
+                    {
+                        endCol = i;
+                    }
+                }
+
+                for (int row = startRow; row <= endRow; row++)
+                {
+                    Console.WriteLine(row);
+                    // if(!File.Exists("./Pics/" + data[row, startCol + 12].Text))
+                    // {
+                    //     Console.WriteLine(data[row, startCol + 12].Text);
+                    // }
+                    apexes.Add(new Apex(data[row, startCol].Text, data[row, startCol + 1].Text, data[row, startCol + 2].Text, data[row, startCol + 3].Text, data[row, startCol + 4].Text, data[row, startCol + 5].Text, data[row, startCol + 6].Text, data[row, startCol + 7].Text, data[row, startCol + 8].Text, data[row, startCol + 9].Text, data[row, startCol + 10].Text, data[row, startCol + 11].Text, data[row, startCol + 12].Text));
+                }
+
+                string output = "apexes = " + JsonConvert.SerializeObject(apexes);
+
+                File.WriteAllText("./Apexes.jsonp", output);
+            }
+
+        }
+    }
+}
